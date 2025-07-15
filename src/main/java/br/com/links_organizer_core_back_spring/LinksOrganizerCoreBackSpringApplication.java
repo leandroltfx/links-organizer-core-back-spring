7b5132b46098841586b0f9fc7a226f392736e702
leandroltfx <leandroltfbr@hotmail.com>
2025-07-15 07:49:52 -0300

    iniciando projeto

---
+++ src/main/java/br/com/links_organizer_core_back_spring/LinksOrganizerCoreBackSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.links_organizer_core_back_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksOrganizerCoreBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksOrganizerCoreBackSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/links_organizer_core_back_spring/LinksOrganizerCoreBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.links_organizer_core_back_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksOrganizerCoreBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
